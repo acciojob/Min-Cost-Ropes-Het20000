843b1ab617469fcad9f2c2f77050334ddd336432
Het20000 <hp985383@gmail.com>
2025-05-10 11:18:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+function mincost(arr) {
+  if (!arr || arr.length === 0) {
+    return 0;
+  }
 
-module.exports=mincost;
+  
+  arr.sort((a, b) => a - b);
+
+  let totalCost = 0;
+  while (arr.length > 1) {
+    
+    const smallest1 = arr.shift();
+    const smallest2 = arr.shift();
+
+
+    const currentCost = smallest1 + smallest2;
+    totalCost += currentCost;
+
+    
+    let inserted = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (currentCost <= arr[i]) {
+        arr.splice(i, 0, currentCost);
+        inserted = true;
+        break;
+      }
+    }
+    if (!inserted) {
+      arr.push(currentCost);
+    }
+  }
+
+  return totalCost;
+}
