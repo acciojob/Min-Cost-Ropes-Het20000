46977ac3a4200f0fee055f66b26e366bff5e1b41
Het20000 <hp985383@gmail.com>
2025-05-11 12:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,72 @@
-function mincost(arr) {
-    arr.sort((a, b) => a - b);
-    let cost = 0;
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
 
-    while (arr.length > 1) {
-        let first = arr.shift();
-        let second = arr.shift();
-        let sum = first + second;
-        cost += sum;
+    insert(val) {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
 
-        // Insert in sorted position
-        let index = arr.findIndex(x => x > sum);
-        if (index === -1) {
-            arr.push(sum);
-        } else {
-            arr.splice(index, 0, sum);
+    extractMin() {
+        if (this.heap.length === 1) return this.heap.pop();
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return min;
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[parentIndex] <= this.heap[index]) break;
+            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+            index = parentIndex;
         }
     }
 
-    return cost;
+    bubbleDown() {
+        let index = 0;
+        while (true) {
+            let leftChild = 2 * index + 1;
+            let rightChild = 2 * index + 2;
+            let smallest = index;
+
+            if (leftChild < this.heap.length && this.heap[leftChild] < this.heap[smallest]) {
+                smallest = leftChild;
+            }
+            if (rightChild < this.heap.length && this.heap[rightChild] < this.heap[smallest]) {
+                smallest = rightChild;
+            }
+            if (smallest === index) break;
+            [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+            index = smallest;
+        }
+    }
+
+    size() {
+        return this.heap.length;
+    }
 }
 
-module.exports = { mincost };
+function mincost(arr) {
+    let heap = new MinHeap();
+    arr.forEach(num => heap.insert(num));
+
+    let totalCost = 0;
+
+    while (heap.size() > 1) {
+        let first = heap.extractMin();
+        let second = heap.extractMin();
+        let cost = first + second;
+        totalCost += cost;
+        heap.insert(cost);
+    }
+
+    return totalCost;
+}
+
+// Example test cases
+console.log(mincost([4, 3, 2, 6]));  // Output: 29
+console.log(mincost([1, 2, 3, 4, 5]));  // Output: 33
