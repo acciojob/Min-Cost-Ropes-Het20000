7257f893119398f8c9c7dbc285308844321676cc
Het20000 <hp985383@gmail.com>
2025-05-11 11:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
-import java.util.PriorityQueue;
+function mincost(arr) {
+    // Turn the array into a min-heap using sort
+    arr.sort((a, b) => a - b);
 
-public class MinCostRopes {
-    public static int mincost(int[] arr) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
-        for (int rope : arr) {
-            minHeap.add(rope);
+    let cost = 0;
+
+    while (arr.length > 1) {
+        // Remove two smallest ropes
+        let first = arr.shift();
+        let second = arr.shift();
+
+        let newRope = first + second;
+        cost += newRope;
+
+        // Insert the new rope and keep array sorted
+        let index = arr.findIndex(x => x > newRope);
+        if (index === -1) {
+            arr.push(newRope);
+        } else {
+            arr.splice(index, 0, newRope);
         }
-        
-        int totalCost = 0;
-        
-        while (minHeap.size() > 1) {
-            int first = minHeap.poll();
-            int second = minHeap.poll();
-            
-            int cost = first + second;
-            totalCost += cost;
-            
-            minHeap.add(cost);
-        }
-        
-        return totalCost;
     }
 
-    public static void main(String[] args) {
-        System.out.println(mincost(new int[]{4, 3, 2, 6})); // Output: 29
-        System.out.println(mincost(new int[]{1, 2, 3, 4, 5})); // Output: 33
-    }
+    return cost;
 }
